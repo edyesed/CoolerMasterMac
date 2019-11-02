a9ebecb78850af8837a81c51f26cfda180e2bc62
Ed Anderson <ed.anderson@outreach.io>
2019-11-02 16:37:01 -0700

    Initial Commit

---
+++ CoolMasterKeys/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoolMasterKeys
+//
+//  Created by Ed Anderson on 1911/02/.
+//  Copyright © 2019 Ed Anderson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
